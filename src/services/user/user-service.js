de499628e3718ef5c453d2a4e50a2354f2e3395b
juhwano <calculus9006@gmail.com>
2022-07-12 12:02:51 +0900

    [Feat] #5 - 유저 조회 구현

--- src/services/user/user-service.js
+++ src/services/user/user-service.js
@@ -2,11 +2,13 @@ import message from '../../utils/response-message.js';
 import statusCode from '../../utils/status-code.js';
 import { response, errResponse } from '../../utils/response.js';
 import User from '../../models/user.js';
+import Bossraid from '../../models/bossraid.js';
+import BossraidRecord from '../../models/bossraid-record.js';
 
 /**
  * @author 강채현
  * @version 1.0 (DB) 유저 회원가입
- * @returns {array<number, response>} response 또는 errResponse 객체
+ * @returns {array<number, object>} statusCode, response 또는 errResponse 객체
  */
 const signUp = async () => {
   try {
@@ -21,6 +23,45 @@ const signUp = async () => {
   }
 };
 
+/**
+ * @author 오주환
+ * @version 1.0 (DB) 유저 조회
+ * @params {string} 유저 id
+ * @returns {array<number, object>} statusCode, response 또는 errResponse 객체
+ */
+const userCheck = async (userId) => {
+  try {
+    const user = await User.findOne({
+      where: { id: userId },
+      include: [
+        {
+          model: Bossraid,
+          include: [
+            {
+              model: BossraidRecord,
+            },
+          ],
+        },
+      ],
+    });
+
+    if (user === null) {
+      return [
+        statusCode.NO_CONTENT,
+        response(statusCode.NO_CONTENT, message.NO_CONTENT),
+      ];
+    } else {
+      return [statusCode.OK, response(statusCode.OK, message.SUCCESS, user)];
+    }
+  } catch (error) {
+    return [
+      statusCode.DB_ERROR,
+      errResponse(statusCode.DB_ERROR, message.DB_ERROR),
+    ];
+  }
+};
+
 export default {
   signUp,
+  userCheck,
 };
