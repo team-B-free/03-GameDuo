c20ce7304f6b01a2f28897321c69051554ad47a7
Woody-Park <starface1009@gmail.com>
2022-07-12 23:52:38 +0900

    [Remove] #4 - 필요없는 폴더 삭제

--- src/routes/bossraid-record/bossraid-record-router.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-import bossraidRecordController from '../../controllers/bossraid-record/bossraid-record-controller.js';
-
-router.get('/', bossraidRecordController.test);
-
-export default router;
