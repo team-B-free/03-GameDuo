98e6e760e07560d5ad54786886ed6f085e1c23f5
Woody-Park <starface1009@gmail.com>
2022-07-13 10:00:11 +0900

    [Refactor] #4 - 코드리뷰 반영하여 적용

--- src/controllers/bossraid/bossraid-controller.js
+++ src/controllers/bossraid/bossraid-controller.js
@@ -2,19 +2,14 @@ import bossRaidService from '../../services/bossraid/bossraid-service.js';
 import statusCode from './../../utils/status-code.js';
 import message from './../../utils/response-message.js';
 
-const getBossRaidInfo = async (req, res) => {
+const getBossRaidInfo = async () => {
   /**
    * @author 박성용
    * @version 1.0 22.07.12 보스레이드 정보 조회 기능
    *
    */
-  try {
-    const bossRaidInfoData = await bossRaidService.bossRaidInfo(req);
-    return res.status(bossRaidInfoData.status).send(bossRaidInfoData);
-  } catch (err) {
-    console.log(err);
-    //throw new Error('에러 발생', { cause: err });
-  }
+  const bossRaidInfoData = await bossRaidService.bossRaidInfo();
+  return res.status(bossRaidInfoData.status).send(bossRaidInfoData);
 };
 
 const bossRaidEnter = async (req, res, next) => {
--- src/services/bossraid/bossraid-service.js
+++ src/services/bossraid/bossraid-service.js
@@ -1,9 +1,10 @@
 import BossRaid from '../../models/bossraid.js';
-import enterCheck from '../../utils/enter-check.js';
+import enterCheck from '../../modules/enter-check.js';
 //import { logger } from '../../config/winston.js';
 import statusCode from '../../utils/status-code.js';
 import message from '../../utils/response-message.js';
 import { errResponse, response } from '../../utils/response.js';
+import { Op } from 'sequelize';
 
 const bossRaidInfo = async (req) => {
   /**
@@ -11,26 +12,25 @@ const bossRaidInfo = async (req) => {
    * @version 1.0 22.07.12 보스레이드 정보 조회 기능
    */
   try {
-    let bossRaidInfo = await BossRaid.findAll({ raw: true });
+    let bossRaidInfo = await BossRaid.findAll();
 
     let canEnter, enteredUserId;
 
-    for (let key in bossRaidInfo) {
+    for (const key in bossRaidInfo) {
       enteredUserId = bossRaidInfo[key].enteredUserId;
 
       // 현재 입장한 유저가 없으면 입장 가능
-      if (enteredUserId === null) {
+      if (!enteredUserId) {
         canEnter = bossRaidInfo[key]['canEnter'] = enterCheck(1);
       } else {
         canEnter = bossRaidInfo[key]['canEnter'] = enterCheck(0);
       }
     }
-    let result = {
+    const data = {
       canEnter: canEnter,
       enteredUserId: enteredUserId,
     };
 
-    let data = result;
     // 성공시
     return response(statusCode.OK, message.SUCCESS, data);
   } catch (err) {
@@ -39,6 +39,13 @@ const bossRaidInfo = async (req) => {
 };
 
 const bossRaidEnter = async (userId, level) => {
+  /**
+   * @author 김영우
+   * @version 1.0 22.07.12 보스레이드 시작 기능
+   *
+   * @author 박성용
+   * @version 1.1 22.07.13 보스레이드 시작시 현재 입장한 유저와 입장여부 업데이트
+   */
   try {
     const bossRaids = await BossRaid.findAll();
     const isEnter = bossRaids.filter(
@@ -47,23 +54,34 @@ const bossRaidEnter = async (userId, level) => {
     )[0];
     if (isEnter) {
       return [statusCode.OK, response(statusCode.OK, { isEntered: false })];
+    } else {
+      const bossRaid = await BossRaid.create({
+        user_id: userId,
+        level,
+        canEnter: false,
+        enteredUserId: userId,
+      });
+      console.log(bossRaid.id);
+
+      await BossRaid.update(
+        { enteredUserId: bossRaid.enteredUserId, canEnter: false },
+        {
+          where: {
+            // enteredUserId >= 1
+            enteredUserId: { [Op.gte]: 1 },
+          },
+        },
+      );
+
+      const data = {
+        isEntered: true,
+        raidRecordId: bossRaid.id,
+      };
+      return [
+        statusCode.CREATED,
+        response(statusCode.CREATED, message.SUCCESS, data),
+      ];
     }
-
-    const bossRaid = await BossRaid.create({
-      user_id: userId,
-      level,
-      canEnter: false,
-      enteredUserId: userId,
-    });
-
-    const data = {
-      isEntered: true,
-      raidRecordId: bossRaid.id,
-    };
-    return [
-      statusCode.CREATED,
-      response(statusCode.CREATED, message.SUCCESS, data),
-    ];
   } catch (err) {
     console.error(err);
     return [
